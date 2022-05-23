a9cae7199d2dae8e2901e10a788d8af8969391f0
Johnson Zhou <johnson@simplyuseful.io>
2022-05-23 12:14:21 +0800

    doc(autonumbersentry): spec

--- AutoNumbersEntry.java
+++ AutoNumbersEntry.java
@@ -9,13 +9,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * Use for auto entry generation in <b>LuckyNumbers</b> competition. 
+ */
 public class AutoNumbersEntry extends NumbersEntry {
     private final int NUMBER_COUNT = 7;
     private final int MAX_NUMBER = 35;
   
+    /**
+     * @param  seed  In the testing mode, you should use the competition 
+     *                identifier as seed for generating the lucky entry and 
+     *                the number of entries in the currently active competition 
+     *                to generate automated customers' entries.
+     */
     public int[] createNumbers (int seed) {
         ArrayList<Integer> validList = new ArrayList<Integer>();
-  int[] tempNumbers = new int[NUMBER_COUNT];
+        int[] tempNumbers = new int[NUMBER_COUNT];
         for (int i = 1; i <= MAX_NUMBER; i++) {
           validList.add(i);
         }
