63968cfee1ce357f3d0d71501b49f619ce340e11
Johnson Zhou <johnson@simplyuseful.io>
2022-05-31 20:34:28 +1000

    test: userInput

--- Test.java
+++ Test.java
@@ -10,5 +10,20 @@
 public class Test {
   public static void main(String[] param) {
     
+    userInput();
+  }
+
+  public static void userInput() {
+    UserConsole console = new UserConsole();
+
+    try {
+      String test = console.readBufferedNext();
+      System.out.println(test);
+      int test2 = console.readBufferedNextInt();
+      System.out.println(test2);
+      System.out.println(console.bufferedSize());
+    } catch(NonNumberException e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
