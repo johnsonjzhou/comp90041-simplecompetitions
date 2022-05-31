93ffd46c9adc5acdc4b1b8a8afc65954d3194dd2
Johnson Zhou <johnson@simplyuseful.io>
2022-05-31 20:26:10 +1000

    test: Test class

---
+++ Test.java
@@ -0,0 +1,14 @@
+/*
+* Student name: Johnson Zhou
+* Student ID: 1302442
+* LMS username: zhoujj
+*/
+
+/**
+ * For testing purposes only 
+ */
+public class Test {
+  public static void main(String[] param) {
+    
+  }
+}
