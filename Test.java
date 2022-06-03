2b906a05df0b24dc29504ed6aa86226ccdcab2d6
Johnson Zhou <johnson@simplyuseful.io>
2022-06-03 20:26:58 +1000

    test: Member

--- Test.java
+++ Test.java
@@ -12,10 +12,12 @@ public class Test {
     
     // userInput();
     // 
-    String[] test = {"111111", "1a1111", "1114345", "12353", "001101"};
-    for (String t : test) {
-      stringMatcher(t);
-    }
+    // String[] test = {"111111", "1a1111", "1114345", "12353", "001101"};
+    // for (String t : test) {
+    //   stringMatcher(t);
+    // }
+    loadMember();
+    
   }
 
   public static void userInput() {
@@ -35,5 +37,22 @@ public class Test {
   public static void stringMatcher(String test) {
     boolean match = test.matches("^\\d{6}$");
     System.out.println(match);
+  }
+
+  public static void loadMember() {
+    String csv = "111111,John,john@abc.com";
+    String csv1 = "11111m,John,john@abc.com";
+    String csv2 = "111111,,john@abc.com";
+    String csv3 = "11111,John,john@abc.com";
+    String csv4 = "111111,John,";
+
+    try {
+      Member m = new Member(csv4);
+      System.out.println(m);
+
+    } catch (DataFormatException e) {
+      System.out.println(e.getCause());
+      System.out.println(e.getMessage());
+    }
   }
 }
