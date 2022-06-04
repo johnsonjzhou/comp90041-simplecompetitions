bff202f2e5e8c76c7ece4d3b11a567a700a8bf65
Johnson Zhou <johnson@simplyuseful.io>
2022-06-04 21:43:09 +1000

    feat: MenuException

---
+++ MenuException.java
@@ -0,0 +1,10 @@
+/*
+* Student name: Johnson Zhou
+* Student ID: 1302442
+* LMS username: zhoujj
+*/
+public class MenuException extends Exception {
+  public MenuException(String message) {
+    super(message);
+  }
+}
