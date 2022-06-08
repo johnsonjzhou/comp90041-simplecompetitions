1cd15990b4983cd2383ca341353e249de26a76c4
Johnson Zhou <johnson@simplyuseful.io>
2022-06-08 15:09:47 +1000

    feat: InvalidEntryException

---
+++ InvalidEntryException.java
@@ -0,0 +1,12 @@
+/*
+* Student name: Johnson Zhou
+* Student ID: 1302442
+* LMS username: zhoujj
+*/
+
+public class InvalidEntryException extends Exception {
+
+  public InvalidEntryException(String message) {
+    super(message);
+  }
+}
