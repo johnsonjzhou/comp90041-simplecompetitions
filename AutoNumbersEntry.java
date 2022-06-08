b0b65efed087ee98118c38e8f735e39fd6388849
Johnson Zhou <johnson@simplyuseful.io>
2022-06-08 15:11:20 +1000

    feat: AutoNumbersEntry initial

--- AutoNumbersEntry.java
+++ AutoNumbersEntry.java
@@ -5,7 +5,6 @@
  */
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
 
@@ -13,26 +12,44 @@ import java.util.Random;
  * Use for auto entry generation in <b>LuckyNumbers</b> competition. 
  */
 public class AutoNumbersEntry extends NumbersEntry {
-    private final int NUMBER_COUNT = 7;
-    private final int MAX_NUMBER = 35;
+
+  public AutoNumbersEntry(int entryId, String billId, String memberId) {
+    super(entryId, billId, memberId);
+  }
   
-    /**
-     * @param  seed  In the testing mode, you should use the competition 
-     *                identifier as seed for generating the lucky entry and 
-     *                the number of entries in the currently active competition 
-     *                to generate automated customers' entries.
-     */
-    public int[] createNumbers (int seed) {
-        ArrayList<Integer> validList = new ArrayList<Integer>();
-        int[] tempNumbers = new int[NUMBER_COUNT];
-        for (int i = 1; i <= MAX_NUMBER; i++) {
-          validList.add(i);
-        }
-        Collections.shuffle(validList, new Random(seed));
-        for (int i = 0; i < NUMBER_COUNT; i++) {
-          tempNumbers[i] = validList.get(i);
-        }
-        Arrays.sort(tempNumbers);
-        return tempNumbers;
+  /**
+   * @param  seed  In the testing mode, you should use the competition 
+   *                identifier as seed for generating the lucky entry and 
+   *                the number of entries in the currently active competition 
+   *                to generate automated customers' entries.
+   */
+  public void createNumbers (int seed) throws InvalidEntryException {
+    ArrayList<Integer> validList = new ArrayList<Integer>();
+    // int[] tempNumbers = new int[this.NUMBER_COUNT];
+    for (int i = 1; i <= this.MAX_NUMBER; i++) {
+      validList.add(i);
     }
+    Collections.shuffle(validList, new Random(seed));
+    for (int i = 0; i < this.NUMBER_COUNT; i++) {
+      // tempNumbers[i] = validList.get(i);
+      this.stage(validList.get(i).intValue());
+    }
+    // Arrays.sort(tempNumbers);
+    // return tempNumbers;
+    this.create();
+  }
+
+  /**
+   * @return  summary of entry id and numbers generated if valid 
+   */
+  @Override
+  public String summary() {
+    String summary = super.summary(); 
+    
+    if (this.isValid()) {
+      summary += " [Auto]";
+    }
+    
+    return summary;
+  }
 }
