6cd31571596739059d4a4e102c187e1e696bf302
Johnson Zhou <johnson@simplyuseful.io>
2022-06-09 18:29:23 +1000

    feat: generate with random seed

--- AutoNumbersEntry.java
+++ AutoNumbersEntry.java
@@ -13,6 +13,8 @@ import java.util.Random;
  */
 public class AutoNumbersEntry extends NumbersEntry {
 
+  private static final int MEANING_OF_LIFE = 42;
+
   public AutoNumbersEntry(int entryId, String billId, String memberId) {
     super(entryId, billId, memberId);
   }
@@ -23,28 +25,28 @@ public class AutoNumbersEntry extends NumbersEntry {
    *                the number of entries in the currently active competition 
    *                to generate automated customers' entries.
    */
-  public void createNumbers (int seed) throws InvalidEntryException {
+  public void create(int seed) throws InvalidEntryException {
     ArrayList<Integer> validList = new ArrayList<Integer>();
-    // int[] tempNumbers = new int[this.NUMBER_COUNT];
     for (int i = 1; i <= this.MAX_NUMBER; i++) {
       validList.add(i);
     }
     Collections.shuffle(validList, new Random(seed));
     for (int i = 0; i < this.NUMBER_COUNT; i++) {
-      // tempNumbers[i] = validList.get(i);
-      this.stage(validList.get(i).intValue());
+      super.stage(validList.get(i).intValue());
     }
-    // Arrays.sort(tempNumbers);
-    // return tempNumbers;
-    this.create();
 
-    // seed = 1, Numbers:  3  4 17 18 24 29 30 [Auto]
-    // seed = 2, Numbers:  2  7 14 18 22 25 35 [Auto]
-    // seed = 3, Numbers:  1  5  6  8 31 32 35 [Auto]
-    // seed = 4, Numbers:  2  5 11 12 23 24 34 [Auto]
-    // seed = 5, Numbers:  1  2 13 24 25 31 34 [Auto]
-    // seed = 6, Numbers:  2  3 15 18 26 27 31 [Auto]
-    // seed = 7, Numbers:  4  6 14 16 17 18 20 [Auto]
+    super.create();
+  }
+
+  /**
+   * In normal mode, create an entry by randoming selecting a seed based on 
+   * the MEANING_OF_LIFE 
+   */
+  @Override
+  public void create() throws InvalidEntryException {
+    Random random = new Random();
+    int seed = random.nextInt(MEANING_OF_LIFE);
+    this.create(seed);
   }
 
   /**
